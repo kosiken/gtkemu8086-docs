b605a1164dd9555569ab1c3fee47bf8f2bc84320
kosiken <allisonkosy@gmail.com>
2020-12-24 12:30:55 +0100

    upd

--- src/@rocketseat/gatsby-theme-docs/components/Logo.js
+++ src/@rocketseat/gatsby-theme-docs/components/Logo.js
@@ -1,6 +1,6 @@
 import React from 'react';
 // Path to the logo file on your project
-import rocketseatLogo from '../../../assets/krc_small.png'; //'assets/krc_small.png';
+import rocketseatLogo from '../../../assets/krc.png'; //'assets/krc_small.png';
 const Logo = () => (
   <img src={rocketseatLogo} alt="Rocketseat logo" style={{ width: 180 }} />
 );
